21b0e2f25bf0cdfad6c8d3e7bfc3aa25d690b887
SDG_Public <124683020+SDG-Public@users.noreply.github.com>
2023-03-14 10:16:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,13 +81,13 @@ def test_script():
     
    outputBlobName	= "test_cancer_PRES_FACT_DET_SP_ADMIN.csv"
        
-   # UPLOAD
-   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)   
-
-   with open(outputBlobName,"w") as csv_file:
-       writer = csv.writer(csv_file, delimiter=';')
-       for line in llista_final:
-           writer.writerow(line)
+   ## UPLOAD
+   #blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)   
+   #
+   #with open(outputBlobName,"w") as csv_file:
+   #    writer = csv.writer(csv_file, delimiter=';')
+   #    for line in llista_final:
+   #        writer.writerow(line)
 
    #with open(outputBlobName, "r") as data:
    #   blob.upload_blob(data)
