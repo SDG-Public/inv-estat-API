9d52af47364a83ab1f7f4405715bfade99108d54
SDG_Public <124683020+SDG-Public@users.noreply.github.com>
2023-03-14 14:01:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,19 @@ import pandas as pd
 
 app = Flask(__name__)
 
-
+# Este controla la pagina inicial de nuestra Web App
 @app.route('/')
 def index():
-
-   return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+   return '¡La App está activa!'
 
 
-@app.route('/run', methods=['GET'])
-def query_records():
-   # Define parameters
-   connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccountdessdg;AccountKey=oy2ydW+f9L+p5SLFSHXvcQsn8yzDzTbzT6YPVNItVwnznodLVYcLsR/FAkI42DSqNCoeGYfJIKXf+AStNMBovw==;EndpointSuffix=core.windows.net"
-   containerName = "inversionsestat"
-   outputBlobName	= "iris_setosa.csv"
-   
-   # Establish connection with the blob storage account
-   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
-    
-   data = 'esto es una prueba de blob'
-    
-   blob.upload_blob(data)
-   
-   return 'Blob subido'
+
    
 @app.route('/arnau', methods=['GET'])
 def test_script():
 
    # Define parameters
-   connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccountdessdg;AccountKey=oy2ydW+f9L+p5SLFSHXvcQsn8yzDzTbzT6YPVNItVwnznodLVYcLsR/FAkI42DSqNCoeGYfJIKXf+AStNMBovw==;EndpointSuffix=core.windows.net"
+   connectionString = os.environ['storage']
    containerName = "inversionsestat"
    inputBlobName = "Detall_SP_Admin.CSV"
    
@@ -48,15 +32,7 @@ def test_script():
    datos = download_stream.readall()
    
    str_datos = datos.decode('cp1252')
-    
-   
-   
-   #
-   #
-   # HASTA AQUI FUNIONA OK!
-   #
-   #
-   
+
    llista_origen = []
    for row in iter(str_datos.splitlines()):
        llista_origen.append(row.split(';'))
@@ -98,6 +74,24 @@ def test_script():
 
    blob.upload_blob(data,overwrite=True)
       
+   return 'Blob subido'
+
+# Con esta petición GET podemos hacer pruebas. Por ejemplo podemos crear un fichero en nuestro Blob Storage
+
+@app.route('/test', methods=['GET'])
+def query_records():
+   # Define parameters
+   connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccountdessdg;AccountKey=oy2ydW+f9L+p5SLFSHXvcQsn8yzDzTbzT6YPVNItVwnznodLVYcLsR/FAkI42DSqNCoeGYfJIKXf+AStNMBovw==;EndpointSuffix=core.windows.net"
+   containerName = "inversionsestat"
+   outputBlobName	= "iris_setosa.csv"
+   
+   # Establish connection with the blob storage account
+   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
+    
+   data = 'esto es una prueba de blob'
+    
+   blob.upload_blob(data,overwrite=True)
+   
    return 'Blob subido'
 
 if __name__ == '__main__':
