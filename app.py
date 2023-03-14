09cd3e9d337dce7a889cad1998574eba0d392666
SDG_Public <124683020+SDG-Public@users.noreply.github.com>
2023-03-14 08:56:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    # Define parameters
-    connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccountdessdg;AccountKey=oy2ydW+f9L+p5SLFSHXvcQsn8yzDzTbzT6YPVNItVwnznodLVYcLsR/FAkI42DSqNCoeGYfJIKXf+AStNMBovw==;EndpointSuffix=core.windows.net"
-    containerName = "inversionsestat"
-    outputBlobName	= "iris_setosa.csv"
+   connectionString = "DefaultEndpointsProtocol=https;AccountName=storageaccountdessdg;AccountKey=oy2ydW+f9L+p5SLFSHXvcQsn8yzDzTbzT6YPVNItVwnznodLVYcLsR/FAkI42DSqNCoeGYfJIKXf+AStNMBovw==;EndpointSuffix=core.windows.net"
+   containerName = "inversionsestat"
+   outputBlobName	= "iris_setosa.csv"
    
    # Establish connection with the blob storage account
-    blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
+   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=outputBlobName)
     
    data = 'esto es una prueba de blob'
-   
-   blob.upload_blob(data)
+    
+   #blob.upload_blob(data)
    
    return json.dumps({'name': 'alice',
                        'email': 'alice@outlook.com'})
