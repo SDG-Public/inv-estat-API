f2e474de346addea474be198f99d8357d0d08d21
SDG_Public <124683020+SDG-Public@users.noreply.github.com>
2023-03-17 11:29:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import csv
 import os
 import pandas as pd
 from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.files.file import File
 from office365.runtime.auth.authentication_context import AuthenticationContext
 
 
