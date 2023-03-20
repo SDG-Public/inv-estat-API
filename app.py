66662db4deb20db3478d581f6ddd0b3f22b868c8
SDG_Public <124683020+SDG-Public@users.noreply.github.com>
2023-03-20 15:30:54 +0100

    test 4

--- app.py
+++ app.py
@@ -9,6 +9,54 @@ from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File
 from office365.runtime.auth.authentication_context import AuthenticationContext
 
+
+# Parametros de conexion Blob Storage
+connectionString = os.environ['CUSTOMCONNSTR_storage']
+containerName = "inversionsestat"
+
+
+def descarga_blob(download_file):
+   # Nos conectamos al Blob Storage
+   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=download_file)
+    
+   # Descargamos el fichero y guardamos su valor en una variable
+   download_stream = blob.download_blob()
+   datos = download_stream.readall()
+   
+   # Decodificamos los datos ANSI (cp1252)
+   str_datos = datos.decode('cp1252')
+    
+   # A partir de los datos generamos una lista
+   llista_descarregada = []
+   for row in iter(str_datos.splitlines()):
+       llista_descarregada.append(row.split(';'))
+       
+   return llista_descarregada
+
+
+def descarga_excel(download_file,posicions_excel):
+   # Nos conectamos al Blob Storage
+   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=download_file)
+    
+   # Descargamos el fichero y guardamos su valor en una variable
+   download_stream = blob.download_blob()
+   datos = download_stream.readall()
+   
+   # read the excel file
+   df = pd.read_excel(datos, posicions_excel)
+       
+   return df
+
+def subida_blob(upload_file,llista_final):
+   # Creamos una conexión con un nuevo nombre de destino
+   blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=upload_file)  
+   
+   # Cargamos los datos a un dataframe
+   df = pd.DataFrame(llista_final[1:],columns = llista_final[0])
+   data = df.to_csv(index=False,sep=";")
+   # Los subimos a Blob Storage
+   blob.upload_blob(data,overwrite=True)
+
 app = Flask(__name__)
 
 # Este controla la pagina inicial de nuestra Web App
